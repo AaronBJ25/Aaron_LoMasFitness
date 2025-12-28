b6c428b327842ec9540d11aee931684550489835
AaronBJ25 <aaronbj86@alumnos.cei.es>
2025-12-28 19:48:43 +0100

    Section added

--- js/index.js
+++ js/index.js
@@ -1,18 +1,18 @@
 //Constantes
-const MENU__BTN = document.getElementById('menuBtn')
-const MENU = document.getElementById('menuLinks')
-const CLOSE__BTN = document.getElementById('closeBtn')
-const BODY = document.querySelector('body')
+const MENU__BTN = document.getElementById('menuBtn');
+const MENU = document.getElementById('menuLinks');
+const CLOSE__BTN = document.getElementById('closeBtn');
+const BODY = document.querySelector('body');
 //Variables
 
 
 //Funciones
 MENU__BTN.addEventListener('click', () => {
-    MENU.classList.add('show')
-    BODY.style.overflow = 'hidden'
-})
+    MENU.classList.add('show');
+    BODY.style.overflowY = 'hidden';
+});
 
 CLOSE__BTN.addEventListener('click', () => {
-    MENU.classList.remove('show')
-    BODY.style.overflow = 'scroll'
-})
+    MENU.classList.remove('show');
+    BODY.style.overflowY = 'scroll';
+});
