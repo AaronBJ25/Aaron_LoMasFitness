9c6b1ad54128b77719035ddfd4c8e264d4e50354
AaronBJ25 <aaronbj86@alumnos.cei.es>
2025-12-29 18:46:09 +0100

    feat: Responsive menu updated

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@
 const MENU__BTN = document.getElementById('menuBtn');
 const MENU = document.getElementById('menuLinks');
 const CLOSE__BTN = document.getElementById('closeBtn');
+const SERVICES__BTN = document.getElementById('servicesBtn');
+const REVIEWS__BTN = document.getElementById('reviewsBtn');
 const BODY = document.querySelector('body');
 //Variables
 
@@ -12,7 +14,19 @@ MENU__BTN.addEventListener('click', () => {
     BODY.style.overflowY = 'hidden';
 });
 
-CLOSE__BTN.addEventListener('click', () => {
+function closeMenu() {
     MENU.classList.remove('show');
     BODY.style.overflowY = 'scroll';
+}
+
+CLOSE__BTN.addEventListener('click', () => {
+    closeMenu();
+});
+
+SERVICES__BTN.addEventListener('click', () => {
+    closeMenu();
+});
+
+REVIEWS__BTN.addEventListener('click', () => {
+    closeMenu();
 });
